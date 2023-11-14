42628b4d2a820119d0db0a55d3c1086b64c825d4
Michael Conard <maconard@mtu.edu>
2023-11-14 15:56:39 -0500

    implement tournament results

--- example_tournament.py
+++ example_tournament.py
@@ -12,4 +12,11 @@ tournament = tilewe.tournament.Tournament([
     tilewe.engine.RandomEngine()
 ])
 
-tournament.play(1000, n_threads=multiprocessing.cpu_count(), move_seconds=15)
+results = tournament.play(10, n_threads=multiprocessing.cpu_count(), move_seconds=15)
+
+# print the result of game 1
+print(results.match_data[0].board)
+
+# print the total real time the tournament took and the average duration of each match, in seconds
+print(f"Tournament ran for {round(results.real_time, 4)}s with avg match duration {round(results.get_average_match_duration(), 4)}s")
+
