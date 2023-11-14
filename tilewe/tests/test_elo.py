f290856c9a83da52ac05b344538bafa02166afcd
Michael Conard <maconard@mtu.edu>
2023-11-14 03:02:50 -0500

    enforce C value in tests in case the default changes

--- tilewe/tests/test_elo.py
+++ tilewe/tests/test_elo.py
@@ -6,26 +6,28 @@ import tilewe.elo
 class TestTilewe(unittest.TestCase): 
     
     def test_elo_win_rate(self): 
+        C = 400
+
         # 50.00% win rate on matching elo
-        self.assertEqual(tilewe.elo.elo_win_probability(1500, 1500), 0.5)
+        self.assertEqual(tilewe.elo.elo_win_probability(1500, 1500, C), 0.5)
 
         # 64.01% win rate against 100 weaker enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 1400), 0.6401, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 1400, C), 0.6401, 4)
 
         # 35.99% win rate against 100 stronger enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1400, 1500), 0.3599, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1400, 1500, C), 0.3599, 4)
         
         # 94.68% win rate against 500 weaker enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(2000, 1500), 0.9468, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(2000, 1500, C), 0.9468, 4)
 
         #  5.32% win rate against 500 stronger enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 2000), 0.0532, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 2000, C), 0.0532, 4)
         
         # 99.01% win rate against 800 weaker enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(2300, 1500), 0.9901, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(2300, 1500, C), 0.9901, 4)
 
         #  0.99% win rate against 800 stronger enemy
-        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 2300), 0.0099, 4)
+        self.assertAlmostEqual(tilewe.elo.elo_win_probability(1500, 2300, C), 0.0099, 4)
 
     def test_elo_rank_change(self):
         K = 32
